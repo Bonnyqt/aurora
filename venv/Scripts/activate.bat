6f65de28c86b4dfada8b906e74092a7664b4d9d7
Bonnyqt <marquezjonbon@gmail.com>
2025-06-26 14:06:24 +0800

    fourth

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\marqu\aurora\venv"
+set "VIRTUAL_ENV=C:\Users\wrafalls\aurora\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
